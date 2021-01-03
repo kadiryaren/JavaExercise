3f2b2a7a31faae437c581ee8056bb438f404a196
Kadir Yaren <uraniumstar20@gmail.com>
2021-01-03 17:40:53 +0300

    Arrays created!

---
+++ Arrays.java
@@ -0,0 +1,20 @@
+// coded by Kadir Yaren
+public class Arrays{
+    public static void main(String[] args) {
+        // define one dimension array
+        int[] a = new int[2];
+        // we defined array who can storage 2 integer value.
+        int[] b = {1,2,3,4,5};
+
+        int[][] c = new int[2][2];
+
+        // multi dimension array 2x2 matris
+
+        // or
+
+        int[][] d = {{1,2,3},{4,5,6}};
+
+        // we can print it with for loop
+
+    }
+}
